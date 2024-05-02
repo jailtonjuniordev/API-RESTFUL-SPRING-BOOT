2bd2e7e28e6435f073e14d815703c09b14b5f150
Jailton Júnior DEV <jailtonjr99@hotmail.com.br>
2024-05-02 20:56:34 -0300

    FEAT ADD: Criação do model de Category e Products

---
+++ src/main/java/br/com/jjdev/APIREST/domain/entity/Category.java
@@ -0,0 +1,47 @@
+package br.com.jjdev.APIREST.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany
+    private List<Product> products;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(nullable = false)
+    private Boolean deleted = false;
+
+}
---
+++ src/main/java/br/com/jjdev/APIREST/domain/entity/Product.java
@@ -0,0 +1,49 @@
+package br.com.jjdev.APIREST.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
+
+    @ManyToOne
+    private Category category;
+
+    private BigDecimal price;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(nullable = false)
+    private Boolean deleted = false;
+}
